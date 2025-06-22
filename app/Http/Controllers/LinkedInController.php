d54b470ae1577951b15002f72c8aec751cf78a38
Rehan Kanak <zrehan286@gmail.com>
2025-06-22 08:18:27 -0400

    moved to api

--- app/Http/Controllers/LinkedInController.php
+++ app/Http/Controllers/LinkedInController.php
@@ -4,16 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Jobs\LinkedInScrapperJob;
 use App\Jobs\ProcessCommentsJob;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 
 class LinkedInController extends Controller
 {
     CONST LINKEDIN = 'LINKEDIN';
-    public function __invoke(): array
+    public function __invoke(Request $request): JsonResponse
     {
-        $linkedIn = "https://www.linkedin.com/posts/vitddnv_i-spent-all-yesterday-building-something-activity-7341809198910816257-caUC?utm_source=share&utm_medium=member_desktop&rcm=ACoAAB72h0MBOnUIO9cft0auMRaOXpe38GB28kU";
+        try {
+            $validator = $request->validate([
+                'url' => ['required', 'url']
+            ]);
 
-        LinkedInScrapperJob::dispatch($linkedIn);
+            $linkedIn = "https://www.linkedin.com/posts/vitddnv_i-spent-all-yesterday-building-something-activity-7341809198910816257-caUC?utm_source=share&utm_medium=member_desktop&rcm=ACoAAB72h0MBOnUIO9cft0auMRaOXpe38GB28kU";
 
-        return [];
+            LinkedInScrapperJob::dispatch($request->url);
+
+            return response()->json([], 200);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+            ], 422);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => "Something went wrong",
+            ], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LinkedInController;
 
+Route::get('/linkedin', LinkedInController::class);
