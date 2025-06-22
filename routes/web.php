db3ce35774aa24f18519892b56b52b75ac7e5ff0
Rehan Kanak <zrehan286@gmail.com>
2025-06-22 06:44:21 -0400

    base done

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LinkedInController;
-use App\Http\Controllers\PersonaController;
 
 Route::get('/linkedin', LinkedInController::class);
-
-Route::get('/', PersonaController::class);
